b0122aaf7240929cd83c82a83ae9f83c6d0e7582
Fahim <alnagfuz0179@gmail.com>
2026-04-23 11:22:44 +0600

    Update CORS for mobile and frontend

--- kothay-server/index.js
+++ kothay-server/index.js
@@ -9,7 +9,12 @@ const watchlistRoutes = require('./routes/watchlist');
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ['https://kothay-client.vercel.app', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqllmqp.mongodb.net/?appName=Cluster0`;
 
